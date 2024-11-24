f57c7c34a0e3c0554c25e6864b5e8b2acae3fecf
Milena K <62288662+xMelaa@users.noreply.github.com>
2024-11-24 16:53:32 +0100

    expanding search bar

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-document
-  .getElementById("colorfulButtonLink")
-  .addEventListener("click", (event) => {
-    event.preventDefault()
+const loadComponent = (id, path) => {
+   document.getElementById(id).addEventListener("click", (event) => {
+      event.preventDefault()
 
-    const mainDiv = document.querySelector(".main")
+      const mainDiv = document.querySelector(".main")
 
-    fetch("./components/colorfulButton/colorfulButton.html")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok " + res.statusText)
-        }
-        return res.text()
-      })
-      .then((html) => {
-        mainDiv.innerHTML = html
-      })
-      .catch((error) => {
-        console.error(
-          "There has been a problem with your fetch operation:",
-          error
-        )
-      })
-  })
+      fetch(path)
+         .then((res) => {
+            if (!res.ok) {
+               throw new Error("Network response was not ok " + res.statusText)
+            }
+            return res.text()
+         })
+         .then((html) => {
+            mainDiv.innerHTML = html
+         })
+         .catch((error) => {
+            console.error(
+               "There has been a problem with your fetch operation:",
+               error
+            )
+         })
+   })
+}
+
+loadComponent(
+   "colorfulButtonLink",
+   "./components/colorfulButton/colorfulButton.html"
+)
+loadComponent("searchBarLink", "./components/searchBar/searchBar.html")
