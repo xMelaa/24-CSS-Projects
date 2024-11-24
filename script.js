46a99145b06d544991776635ef9a117ebac6caa7
Milena K <62288662+xMelaa@users.noreply.github.com>
2024-11-24 20:27:34 +0100

    codepen tile

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 const loadComponent = (id, path) => {
-   document.getElementById(id).addEventListener("click", (event) => {
-      event.preventDefault()
+  document.getElementById(id).addEventListener("click", (event) => {
+    event.preventDefault()
 
-      const mainDiv = document.querySelector(".main")
+    const mainDiv = document.querySelector(".main")
 
-      fetch(path)
-         .then((res) => {
-            if (!res.ok) {
-               throw new Error("Network response was not ok " + res.statusText)
-            }
-            return res.text()
-         })
-         .then((html) => {
-            mainDiv.innerHTML = html
-         })
-         .catch((error) => {
-            console.error(
-               "There has been a problem with your fetch operation:",
-               error
-            )
-         })
-   })
+    fetch(path)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok " + res.statusText)
+        }
+        return res.text()
+      })
+      .then((html) => {
+        mainDiv.innerHTML = html
+      })
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        )
+      })
+  })
 }
 
 loadComponent(
-   "colorfulButtonLink",
-   "./components/colorfulButton/colorfulButton.html"
+  "colorfulButtonLink",
+  "./components/colorfulButton/colorfulButton.html"
 )
 loadComponent("searchBarLink", "./components/searchBar/searchBar.html")
+loadComponent("codePenLink", "./components/codepenTile/codepenTile.html")
